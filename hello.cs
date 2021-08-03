53566766d85f7832101319b5e6c4012f2e71a869
Durhy <87582044+durhydev@users.noreply.github.com>
2021-08-03 12:13:54 -0400

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,10 @@
+using System;
+
+// the "namespace" is used for organization the program
+namespace file{
+	class hello{
+		static void Main(string[] args){
+			Console.WriteLine("Hello world!");
+		}
+	}
+}
