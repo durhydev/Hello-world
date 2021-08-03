2f23d563dc6098d0b4d987b3340429e2f75aa21a
Durhy <87582044+durhydev@users.noreply.github.com>
2021-08-03 12:20:55 -0400

    Create hello4.cs

---
+++ hello4.cs
@@ -0,0 +1,19 @@
+using System;
+
+class hello4{
+	static void Main(string[] args){
+		Console.Write("H");
+		Console.Write("E");
+		Console.Write("L");
+		Console.Write("L");
+		Console.Write("O");
+		Console.Write(" ");
+		Console.Write("W");
+		Console.Write("O");
+		Console.Write("R");
+		Console.Write("L");
+		Console.Write("D");
+		Console.Write("!");
+		Console.Write("\n");
+	}
+}
