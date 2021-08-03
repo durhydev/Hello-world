3f36cf76905c626c7c45ff2be039abcf9365c936
Durhy <87582044+durhydev@users.noreply.github.com>
2021-08-03 12:24:50 -0400

    Create hello5.cs

---
+++ hello5.cs
@@ -0,0 +1,13 @@
+using System; // input and output
+
+namespace file{
+	class hello5{
+		static void Main(string[] args){
+			string message = "Hello world!";
+			Console.WriteLine(message);
+			
+			byte len = 12;
+			Console.WriteLine("Length: " + len);
+		}
+	}
+}
